8f082d7800803e000c7ca4670427768db07df595
weiki <wen18826478504@163.com>
2018-10-22 07:48:55 +0800

    新闻

---
+++ system/config/database.php
@@ -0,0 +1,13 @@
+<?php
+
+$config['charset'] = 'utf8';
+$config['prefix'] = 'travel_';
+$config['driver'] = 'MySQLi';
+$config['master'] = array (
+  'charset' => 'utf8',
+  'host' => '59.110.216.211',
+  'username' => 'root',
+  'password' => '9GGeuex1Nv8E',
+  'dbname' => 'travel',
+);
+$config['slave'] = false;
